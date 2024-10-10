26f9db38713b0e6deaed1a5cb1b4afb50d77b5ae
pedroluccabr <pedrolccmartins@gmail.com>
2024-10-10 19:45:09 -0300

    First commit
    Primeiro passo a passo pronto para começar a codar.

---
+++ pacezap.py
@@ -0,0 +1,13 @@
+# tela inicial:
+    # titulo
+    # botao para iniciar o chat
+        # quando clicar no botao abrir um pop up
+            # um titulo "bem vindo ao pacezap"
+            # uma caixa de texto para escrever o nome no chat
+            # um botao para entrar no site  
+                # mudar o titulo
+                # sumir com o pop up
+                # carregar o chat  
+                # carregar o espaco de digitar a msg
+                # botao para enviar as msgs
+                    # quando clicar no botao alem de enviar limpa a msg
